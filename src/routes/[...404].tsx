aaadbadcd3251a2c9dd7a81b9eccb4072a869c8d
Liam Brewer <liam.a.brewer@gmail.com>
2023-01-26 22:16:21 -0600

    Center 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,7 +2,7 @@ import { A } from 'solid-start';
 
 export default function NotFound() {
   return (
-    <main class='flex h-screen w-screen flex-col items-center justify-center'>
+    <main class='-mt-16 flex h-screen w-full flex-col items-center justify-center'>
       <h2 class='text-2xl font-bold'>404 Not Found</h2>
       <p>The requested resource does not exist on this server.</p>
       <A href='/' class='mt-4 text-lg text-blue-500 hover:underline'>
