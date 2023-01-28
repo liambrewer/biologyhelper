696b4acde610ba881b1bfeb391e22ec9ab18271b
Liam Brewer <liam.a.brewer@gmail.com>
2023-01-27 23:11:15 -0600

    Make index helpers list look a little nicer

--- src/root.tsx
+++ src/root.tsx
@@ -36,7 +36,7 @@ export default function Root() {
         ></Link>
       </Head>
       <Body>
-        <div class='min-h-screen'>
+        <div class='flex min-h-screen flex-col'>
           <Navbar />
           <Suspense>
             <ErrorBoundary>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,44 @@
+import { For } from 'solid-js';
 import { A } from 'solid-start';
+
+interface HelperLink {
+  href: string;
+  label: string;
+}
+
+const links: HelperLink[] = [
+  {
+    href: '/mrna-transcription',
+    label: 'mRNA Transcription',
+  },
+  {
+    href: '/nitrogenous-bases',
+    label: 'Nitrogenous Bases',
+  },
+];
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <A href='/mrna-transcription' class='text-blue-500 hover:underline'>
-          mRNA Transcription
-        </A>
-      </li>
-      <li>
-        <A href='/nitrogenous-bases' class='text-blue-500 hover:underline'>
-          Nitrogenous Bases
-        </A>
-      </li>
-    </ul>
+    <div class='container mx-auto'>
+      <div class='my-10 flex flex-col overflow-clip rounded-lg bg-green-500 text-white'>
+        <div class='flex w-full items-center border-b-2 p-4'>
+          <h2 class='text-2xl font-bold'>Helpers</h2>
+        </div>
+        <ul class='flex-grow divide-y'>
+          <For each={links}>
+            {(link) => (
+              <li class='group flex w-full items-stretch hover:bg-green-400'>
+                <A
+                  href={link.href}
+                  class='w-full py-2 px-4 text-lg group-hover:underline'
+                >
+                  {link.label}
+                </A>
+              </li>
+            )}
+          </For>
+        </ul>
+      </div>
+    </div>
   );
 }
